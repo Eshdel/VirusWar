ee3b10ae2001d707561c59efe2a9bab5492b75dc
Eshdel <gagin_ilya@list.ru>
2022-12-18 02:01:38 +0700

    Prototype InputHandler

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,9 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputHandler : MonoBehaviour
 {
-    public static event System.Action<Ray, RaycastHit2D> LeftClicked;
-    public static event System.Action<Ray, RaycastHit2D> RightClicked;
+    public static UnityEvent<EnemyCell> OnClickEnemyCell;
+    public static UnityEvent<PlayerCell> OnClickPlayerCell;
+
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit raycastHit;
+
+            if(Physics.Raycast(ray,out raycastHit))
+            {
+                var target = raycastHit.collider.transform.gameObject;
+                var cell = target.GetComponent<Cell>();
+
+                switch (cell) {
+                    case EnemyCell: {
+                        Debug.Log(" Enemy Hit");
+                        OnClickEnemyCell.Invoke(cell as EnemyCell);
+                        break;
+                    }
+
+                    case PlayerCell: { 
+                        Debug.Log("Player Hit"); 
+                        OnClickPlayerCell.Invoke(cell as PlayerCell);
+                        break;
+                    }
+
+                    default: { 
+                        break;
+                    }
+                }    
+            }
+        }
+    }
 }
