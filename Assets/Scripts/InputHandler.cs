5e606985e46c88ece53e46f131d2122bb7a1988a
Miarel <larsidova@mail.ru>
2022-12-18 00:20:16 +0500

    input handler fix

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -7,35 +7,43 @@ public class InputHandler : MonoBehaviour
 {
     public static UnityEvent<EnemyCell> OnClickEnemyCell;
     public static UnityEvent<PlayerCell> OnClickPlayerCell;
+    public static UnityEvent<NeutralCell> OnClickNeutralCell;
 
     
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log("button down");
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D raycastHit = Physics2D.Raycast(worldPoint, Vector2.zero);
 
-            RaycastHit raycastHit;
-
-            if(Physics.Raycast(ray,out raycastHit))
+            if(raycastHit.collider!=null)
             {
+                Debug.Log("raycasted");
                 var target = raycastHit.collider.transform.gameObject;
                 var cell = target.GetComponent<Cell>();
 
                 switch (cell) {
                     case EnemyCell: {
                         Debug.Log(" Enemy Hit");
-                        OnClickEnemyCell.Invoke(cell as EnemyCell);
+                        OnClickEnemyCell?.Invoke(cell as EnemyCell);
                         break;
                     }
 
                     case PlayerCell: { 
                         Debug.Log("Player Hit"); 
-                        OnClickPlayerCell.Invoke(cell as PlayerCell);
+                        OnClickPlayerCell?.Invoke(cell as PlayerCell);
                         break;
                     }
 
+                    case NeutralCell: { 
+                        Debug.Log("Neutral Hit"); 
+                        OnClickNeutralCell?.Invoke(cell as NeutralCell);
+                        break;
+                    }
                     default: { 
+                        Debug.Log("???");
                         break;
                     }
                 }    
